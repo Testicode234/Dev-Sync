c5f4b3eda7872c68b13a2bd5ec62ba56490f3da5
Testicode234 <xprogrammerr@gmail.com>
2025-03-15 19:00:40 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from './App.tsx';
@@ -10,4 +10,4 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
       <App />
     </BrowserRouter>
   </React.StrictMode>
-);
+);*/
